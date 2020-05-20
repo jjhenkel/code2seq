dd80fff02aa8cd9d05f6781a8309f8397e69f669
Jordan J. Henkel <jjhenkel@cs.wisc.edu>
2020-05-20 09:49:36 -0500

    Fix bug during training. Dont compute rouge.

--- model.py
+++ model.py
@@ -225,8 +225,9 @@ class Model:
 
         elapsed = int(time.time() - eval_start_time)
         precision, recall, f1 = self.calculate_results(true_positive, false_positive, false_negative)
-        files_rouge = FilesRouge(predicted_file_name, ref_file_name)
-        rouge = files_rouge.get_scores(avg=True, ignore_empty=True)
+        # files_rouge = FilesRouge(predicted_file_name, ref_file_name)
+        # rouge = files_rouge.get_scores(avg=True, ignore_empty=True)
+        rouge = 0
         print("Evaluation time: %sh%sm%ss" % ((elapsed // 60 // 60), (elapsed // 60) % 60, elapsed % 60))
         return num_correct_predictions / total_predictions, \
                precision, recall, f1, rouge
